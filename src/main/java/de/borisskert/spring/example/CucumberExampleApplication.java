66c898c2e63db46b7fe9ef6b3f29fc298ac430be
Boris Skert <boris.skert@gmail.com>
2020-04-08 18:35:25 +0200

    Create project with Spring Initializr

---
+++ src/main/java/de/borisskert/spring/example/CucumberExampleApplication.java
@@ -0,0 +1,13 @@
+package de.borisskert.spring.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CucumberExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CucumberExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/borisskert/spring/example/CucumberExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package de.borisskert.spring.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CucumberExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
